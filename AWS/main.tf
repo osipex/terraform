5bc46e78788e753584e4ebd988362b05ee379267
Petro Osypenko <osypenkp@gmail.com>
2020-02-06 11:18:40 +0000

    AWS networking as a module

--- AWS/main.tf
+++ AWS/main.tf
@@ -8,3 +8,11 @@ module "storage" {
   source       = "./storage"
   project_name = "${var.project_name}"
 }
+
+# Deploy Networking Resources
+module "networking" {
+  source       = "./networking"
+  vpc_cidr     = "${var.vpc_cidr}"
+  public_cidrs = "${var.public_cidrs}"
+  accessip     = "${var.accessip}"
+}
